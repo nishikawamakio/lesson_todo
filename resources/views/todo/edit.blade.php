69b6d87a57004f543961ec1690f7d1621b557f64
nishikawamakio <nishikawa.makiogizumo@gmail.com>
2017-05-11 18:59:46 +0900

    lesson29

--- resources/views/todo/edit.blade.php
+++ resources/views/todo/edit.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
-
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-
-    <!-- Fonts -->
-    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-</head>
-<body>
+@extends('app')
+@section('content')
 <div class="container">
     <h2 class="page-header">ToDo編集</h2>
     {!! Form::open(['route' => ['todo.update', $todo->id], 'method' => 'PUT']) !!}
@@ -24,5 +12,5 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </div>
-</body>
-</html>
+
+@endsection
